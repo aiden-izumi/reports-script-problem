6cf5a0a1e45114f08d6eac0e617fa75b963838b6
Aiden Izumi <m.kozhinov@haulmont.com>
2021-11-19 13:40:56 +0400

    Initial commit

---
+++ src/main/java/com/company/reports_script_problem/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.company.reports_script_problem.screen.user;
+
+import com.company.reports_script_problem.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
